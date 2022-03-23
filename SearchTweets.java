4ea8cee1185e898abf403226d7b7b55b1db29f27
Beach Arcade <61742414+BeachArcade@users.noreply.github.com>
2022-03-23 15:44:58 +0000

    Add files via upload

---
+++ SearchTweets.java
@@ -0,0 +1,13 @@
+package project;
+
+import java.io.*;
+
+
+public class SearchTweets {
+
+    public static void main(String[] args) throws IOException{
+        TweetGatherer tweetGatherer = new TweetGatherer();
+        tweetGatherer.startQuery();
+
+    }
+}
